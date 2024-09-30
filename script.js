ab1906b211842ff50b29adaa09d07d7b886e51c0
Md Fahad Farazi <arfanfahad40@gmail.com>
2024-09-30 23:24:08 +0600

    need to fix a lot of things

--- script.js
+++ script.js
@@ -2,44 +2,65 @@
 
 
 
-function total () {
+function add () {
     let item = document.querySelector("#addItem").value; 
     let qty = document.querySelector("#quantity").value; 
     let price = document.querySelector("#price").value; 
 
-
+    
     /*  Item name crated   */
-    let create_items = document.createElement("td"); 
-    create_items.textContent = item; 
-    let item_name = document.getElementById("items"); 
-    item_name.appendChild(create_items); 
+    // let create_items = document.createElement("td"); 
+    // create_items.textContent = item; 
+    let item_table = document.getElementById("items"); 
+    // const newItem = item_table.insertRow(); 
+    // const nameCell = newItem.insertCell(0);
+    // const qtyCell = newItem.insertCell(1); 
+    // const priceCell = newItem.insertCell(2); 
+    // const totalCell = newItem.insertCell(3); 
 
+    // nameCell.innerHTML = item; 
+    // qtyCell.innerHTML = qty; 
+    // priceCell.innerHTML = price; 
+    // totalCell.innerHTML = qty * price; 
 
-    /* quantity  */ 
-    let total_quantity = document.createElement("td"); 
-    total_quantity.textContent = Number(qty); 
-    let totalQuantity = document.getElementById("items");
-    totalQuantity.appendChild(total_quantity); 
+    item_table.innerHTML += `
+         <tr>
+                    <td class="border-2 bg-blue-500 px-2 w-96 py-1">${item}</td>
+                    <td class="border-2 w-44 py-1">${qty}</td>
+                    <td class="border-2 w-44 py-1">${price}</td>
+                    <td class="border-2 w-56 py-1">${qty * price}</td>
+                
+        </tr>
+        `
+    
 
-    /* total price */
-    let item_price = document.createElement("td");
-    item_price.textContent = Number(price);
-    let total_price = document.getElementById("items");
-    total_price.appendChild(item_price); 
+    // /* quantity  */ 
+    // let total_quantity = document.createElement("td"); 
+    // total_quantity.textContent = Number(qty); 
+    // let totalQuantity = document.getElementById("items");
+    // totalQuantity.appendChild(total_quantity); 
 
-    /*  total calculations   */
-    let sum = qty * price; 
-    // console.log(sum.toFixed(2) + " Taka"); 
-    let items_total_price = document.createElement("td"); 
-    items_total_price.textContent = sum.toFixed(2) + " Taka"; 
-    let itemsTotalPrice = document.getElementById("items"); 
-    itemsTotalPrice.appendChild(items_total_price); 
+    // /* total price */
+    // let item_price = document.createElement("td");
+    // item_price.textContent = Number(price);
+    // let total_price = document.getElementById("items");
+    // total_price.appendChild(item_price); 
+
+    // /*  total calculations   */
+    // let sum = qty * price; 
+    // // console.log(sum.toFixed(2) + " Taka"); 
+    // let items_total_price = document.createElement("td"); 
+    // items_total_price.textContent = sum.toFixed(2) + " Taka"; 
+    // let itemsTotalPrice = document.getElementById("items"); 
+    // itemsTotalPrice.appendChild(items_total_price); 
 
 
     /* emptying the input box */
     document.querySelector("#addItem").value = "" ; 
     document.querySelector("#quantity").value = ""; 
     document.querySelector("#price").value = ""; 
+    
+
     
 }
 
